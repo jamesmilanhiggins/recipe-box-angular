874e692c0f3e367c83f260d74a99546622098525
Aubrey Masten and James Higgins <aubrey@>
2017-06-06 10:21:14 -0700

    add one way property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Component} from '@angular/core';
     <div class="container">
       <h1>Recipe Box</h1>
       <ul>
-        <li *ngFor="let recipe of recipes">
+        <li [class]="priorityColor(recipe)" *ngFor="let recipe of recipes">
           <h3>{{recipe.title}}</h3><button (click)="editRecipe(recipe)">Edit Recipe!</button>
           <p>Ingredients:<p>
           <ul>
@@ -24,14 +24,24 @@ import { Component} from '@angular/core';
 
 export class AppComponent {
   recipes: Recipe[] = [
-    new Recipe("Chocolate Chip Cookies", "Mix Mix Mix", ["Milk", "Eggs", "Love"]),
-    new Recipe("Potato Chip Cookies", "Mix Mix Mix", ["Milk", "Eggs", "Love"])
+    new Recipe("Chocolate Chip Cookies", "Mix Mix Mix", ["Milk", "Eggs", "Love"], 3),
+    new Recipe("Potato Chip Cookies", "Mix Mix Mix", ["Milk", "Eggs", "Love"], 1),
+    new Recipe("Tortilla Chip Cookies", "Mix Mix Mix", ["Milk", "Eggs", "Love"], 2)
   ];
   editRecipe(recipe) {
     alert(`You want to edit ${recipe.title}`);
   }
+  priorityColor(recipe) {
+    if (recipe.priority === 3){
+      return "bg-danger";
+    } else if (recipe.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
 }
 
 export class Recipe {
-  constructor(public title: string, public directions: string, public ingredients: string[]){}
+  constructor(public title: string, public directions: string, public ingredients: string[], public priority: number){}
 }
